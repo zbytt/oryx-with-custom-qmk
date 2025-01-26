8a5818fc59b76795ee561e00b64d345f28519201
ZT <zbyszektuszynsky@gmail.com>
2025-01-26 21:16:35 +0100

    achordion config

--- jY5j4/keymap.c
+++ jY5j4/keymap.c
@@ -135,4 +135,14 @@ void housekeeping_task_user(void) {
   achordion_task();
 }
 
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // Also allow same-hand holds when the other key is in the rows outside the
+  // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboards are split.
+  uint8_t row = other_record->event.key.row % (MATRIX_ROWS / 2);
+  if (!(1 <= row && row <= 3)) { return true; }
 
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
